dd2f41c69f349316fde589a3544eb3420bca49fc
Eyob <eabebe91@gmail.com>
2022-06-18 18:52:03 +0300

    start files

---
+++ src/components/NavBar.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref, watchEffect } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const navLinks = ref('element')
+const hamberger = ref('element')
+const navLink = ref('element')
+
+function toggleSidebar() {
+    if (navLinks.value.dataset.visibility === 'false') {
+        navLinks.value.dataset.visibility = 'true'
+        hamberger.value.setAttribute('aria-expanded', true)
+    }
+
+    else {
+        navLinks.value.dataset.visibility = 'false'
+        hamberger.value.setAttribute('aria-expanded', false)
+    }
+}
+
+function navLinkClicked() {
+    navLinks.value.dataset.visibility = 'false'
+    hamberger.value.setAttribute('aria-expanded', false)
+}
+</script>
+
+
+<template>
+    <nav>
+        <RouterLink class="logo-text" to="/">Eyob <span>.</span></RouterLink>
+
+        <ul ref="navLinks" class="nav-links" data-visibility="false">
+            <li @click="navLinkClicked" class="nav-link">
+                <RouterLink class="link" to="/">Home</RouterLink>
+            </li>
+            <li @click="navLinkClicked" class="nav-link">
+                <RouterLink class="link" to="/about">About</RouterLink>
+            </li>
+            <li @click="navLinkClicked" class="nav-link">
+                <RouterLink class="link" to="/projects">Projects</RouterLink>
+            </li>
+        </ul>
+
+        <div ref="hamberger" aria-expanded="false" @click="toggleSidebar" class="hamberger">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+@import '../assets/nav-bar.css';
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () => import("../views/About.vue"),
+    },
+    {
+      path: "/projects",
+      name: "projects",
+      component: () => import("../views/Projects.vue"),
+    },
+  ],
+  linkActiveClass: 'nav-link-active'
+});
+
+export default router;
