712c4386c84678497e903a3b75a9778e86736a57
Eyob <eabebe91@gmail.com>
2022-06-19 11:19:09 +0300

    adding Home page and partial About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,95 @@
 <template>
-    <h1>About</h1>
+    <div id="about">
+        <h1 class="title">About</h1>
+        <div class="about-me">
+            <img src="../assets/images/background.jpg" alt="My Image">
+            <div class="info">
+                <h3>Fullstack JavaScript and Mobile App Developer</h3>
+                <p>My Personal Informations</p>
+                <div class="personal-info">
+                    <p><span>Birthday:</span> 15 Nov 2000</p>
+                    <p><span>Email:</span> eabebe91@gmail.com</p>
+                    <p><span>Phone:</span> +251 964643164</p>
+                    <p><span>City:</span> Gondar, Amhara, Ethiopia</p>
+                    <p><span>Age:</span> 21</p>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+#about {
+    margin: 1.5rem;
+}
+
+.title {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-transform: uppercase;
+}
+
+.title::after {
+    content: '';
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+    display: block;
+    height: 4px;
+    width: 5rem;
+    background-color: #34b7a7;
+    border-radius: 5px;
+}
+
+.about-me {
+    display: flex;
+    gap: 1rem;
+}
+
+.about-me img {
+    width: 40%;
+}
+
+.info {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+}
+
+.info > p{
+    font-weight: 600;
+
+}
+
+.personal-info {
+    /* flex-grow: 2; */
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+}
+
+@media (max-width: 850px) {
+
+    #about {
+        margin: 2rem 5vw;
+    }
+    .about-me {
+        flex-direction: column;
+    }
+
+    .about-me img {
+        width: 100%;
+    }
+}
+
+@media (max-width: 550px) {
+    .personal-info {
+        grid-template-columns: 1fr;
+        gap: 1rem;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+
+const date = new Date();
+
+</script>
+
+
 <template>
-    <h1>Home</h1>
+    <div id="home">
+        <div class="container">
+            <div class="text">
+                <h1>Eyob Abebe Melese</h1>
+                <h3>I'm a Fullstack JavaScript developer</h3>
+            </div>
+            <RouterLink class="about-btn" to="/about">About me</RouterLink>
+        </div>
+        <div class="footer">
+            <p>© Copyright Eyob {{ date.getFullYear() }}. All Rights Reserved</p>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+@import '../assets/styles/home.css'
+</style>
